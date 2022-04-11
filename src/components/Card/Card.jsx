644d4210ddd41bda2177fb772a4a60733dbbb0b9
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-11 20:52:51 -0300

    template basico de una Card de producto, falta la lógica

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,21 @@
-export default function Card (){
+import React from "react";
+
+export default function CardProduct(/*nombre, precio, delivery, */){ //deberia recibir props para renderizar segun los productos
+
     return (
         <div>
-            Hola
+            <div>
+                <img src="https://ferreira.vteximg.com.br/arquivos/ids/226134-588-588/to_21871.jpg?v=636615531533330000" with="60px" alt="producto"/>
+            </div>
+            <div> Nombre </div>
+            <div> Precio </div>
+            <div>
+                <img src="" alt="carrito"/>
+            </div>
+            <div>
+                <img src="" alt="delivery" />
+            </div>
+
         </div>
     )
 }
