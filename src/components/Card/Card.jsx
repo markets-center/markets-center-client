df4057ec8ffeba89f7c124d50485d5f43e398969
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-11 22:17:19 -0300

    Agregado funcionalidad hover para ver + detalle

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-
+import React, { useState } from "react";
+import s from './Card.module.css'
 export default function CardProduct(/*nombre, precio, delivery, */){ //deberia recibir props para renderizar segun los productos
+    const [hover,setHover] = useState(false);
 
+    function moreInfo(e){
+        setHover(true)
+    }
+    function lessInfo(e){
+        setHover(false)
+    }
     return (
-        <div>
+        <div onMouseEnter={moreInfo} onMouseLeave={lessInfo} className={s.container}>
             <div>
-                <img src="https://ferreira.vteximg.com.br/arquivos/ids/226134-588-588/to_21871.jpg?v=636615531533330000" with="60px" alt="producto"/>
+                <img src="https://ferreira.vteximg.com.br/arquivos/ids/226134-588-588/to_21871.jpg?v=636615531533330000" width="200px" alt="producto"/>
             </div>
             <div> Nombre </div>
             <div> Precio </div>
@@ -15,7 +22,13 @@ export default function CardProduct(/*nombre, precio, delivery, */){ //deberia r
             <div>
                 <img src="" alt="delivery" />
             </div>
-
+            {hover?<div>
+                <div> Vendedor </div>
+                <div> Descripcion del producto </div>
+                <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Error laudantium corporis qui sapiente ipsam quam sequi tempore temporibus. Repellendus, beatae dolorum ex unde sed eius mollitia repellat quisquam aliquam numquam! </div>
+                 </div>:""}
         </div>
+        
+
     )
 }
