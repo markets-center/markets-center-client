803e1d742223b1d28efd5093c769ac67754cf418
unknown <ryavar.montesinos@gmail.com>
2022-04-12 08:12:39 -0500

    Package.json(Integracion material UI), change style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,41 @@
-import "./Footer.css";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
 
 export default function Footer() {
   return (
-    <div className="footer-container">
-      <h6>
-        <p>&copy; 2022 Markets-Center</p>
-      </h6>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 3,
+          mt: "auto",
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth="xs">
+          <Typography variant="body2" color="text.secondary">
+            {"Copyright © "}
+            <Link color="inherit" href="/home">
+              Markets Center
+            </Link>
+            {" "}
+            {"2022"}
+            {"."}
+          </Typography>
+        </Container>
+      </Box>
+    </Box>
   );
 }
