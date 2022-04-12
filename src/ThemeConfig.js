3f5dee956f438fd863e6d43e9ea932681fbca232
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-12 12:26:18 -0300

    creacion patela de colores definida global

---
+++ src/ThemeConfig.js
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+          main: '#005BAA',
+        },
+        secondary: {
+          main: '#E2001A',
+        },
+        background: {
+          default: '#F8F9FA',
+        },
+        info: {
+          main: '#2ad833',
+        },
+      },
+});
+
