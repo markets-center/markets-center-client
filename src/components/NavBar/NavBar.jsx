7a3f75916bb07524d43306533e7fd2e73c291caa
Tomas Bohn Guixeras <bohnguixeras@gmail.com>
2022-04-13 10:29:44 -0300

    Navigation function to Buttons(Home, Cart & User)

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import styles from './NavBar.module.css'
 import SearchBar from './SearchBar.jsx'
 import Logo from '../../images/MC-Full.png'
-import styles from './NavBar.module.css'
 
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
@@ -9,16 +9,18 @@ import Toolbar from '@mui/material/Toolbar';
 import { IconButton, Stack } from '@mui/material/'
 import {ShoppingCart, AccountCircle } from '@mui/icons-material';
 
+import { useNavigate } from 'react-router-dom'
 
 
 export default function NavBar(){
+    const navigate = useNavigate()
 
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
 
-                    <IconButton cursor="pointer">
+                    <IconButton cursor="pointer" onClick={() => navigate('/')}>
                         <img src={Logo} alt="Logo" className={styles.logo} />
                     </IconButton>
 
@@ -28,7 +30,7 @@ export default function NavBar(){
                     <IconButton
                         variant="contained"
                         color="white"
-                        to="/usuer"
+                        onClick={() => navigate('/User')}
                     >
                         <AccountCircle fontSize="large"/>
                     </IconButton>
@@ -36,7 +38,7 @@ export default function NavBar(){
                     <IconButton
                         variant="contained"
                         color="white"
-                        to="/usuer"
+                        onClick={() => navigate('/Carrito')}
                     >
                         <ShoppingCart fontSize="large"/>
                     </IconButton>
