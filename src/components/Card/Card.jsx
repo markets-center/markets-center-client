0b152586d987caae2e040598cc1c94ea76279260
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-13 00:16:54 -0300

    Card.jsx: Agregado Modal para cada producto

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,12 +4,31 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { IconButton } from "@mui/material";
 import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
 import s from './Card.module.css'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Detail from './Detail/Detail'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  bgcolor: 'background.paper',
+  border: '1px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
-export default function CardProduct(name, price,image, description){ //deberia recibir props para renderizar segun los productos
+export default function CardProduct(name, price,image, description, stock,){ //deberia recibir props para renderizar segun los productos
    
     const [hover,setHover] = useState(false); 
-
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
     function moreInfo(e){
         setHover(true)
     }
@@ -23,15 +42,15 @@ export default function CardProduct(name, price,image, description){ //deberia r
     return (
         <div onMouseEnter={moreInfo} onMouseLeave={lessInfo} className={s.container}>
             <div className={s.img}>
-                <img src={image} width="200px" alt="producto"/>
+                <img src="https://ferreira.vteximg.com.br/arquivos/ids/226134-588-588/to_21871.jpg?v=636615531533330000" width="200px" alt="producto"/>
             </div>
             <div className={s.infoContainer}>
                 <Typography variant="h6" >
-                   {name}
+                   Zapatilla topper
                 </Typography>
                 <div className={s.priceAndButton}>
                     <Typography variant="subtitle1" className={s.price}>
-                        {price}
+                        1000
                     </Typography>  
                     <div className={s.icons}> 
                         <IconButton color="primary" size="small" onClick={addToCart} > 
@@ -43,8 +62,27 @@ export default function CardProduct(name, price,image, description){ //deberia r
                     </div>
                 </div>
                 {hover?<div> 
-                    <Typography variant="body2" className={s.description}>{description} </Typography>
-                    </div>:""}
+                            <div>
+                            <Typography variant="body2" className={s.description}>Stock: 3 </Typography>
+                            </div>
+                            <Typography variant="body2" className={s.description}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. </Typography>
+                            <div className={s.masinfo}>
+                                <Button  variant="outlined" size="small" color="info" onClick={handleOpen} >más info</Button>
+                            </div>
+                       </div>:""}
+            </div>
+            <div>
+                
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Detail name={name} price={price} image={image} stock={stock} description={description}/>
+                    </Box>
+                </Modal>
             </div>
         </div>
         
