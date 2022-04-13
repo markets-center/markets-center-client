d299bf4792e1ba4b6ef01ac8f31bd841b143473a
FNahuelO <franconawel@gmail.com>
2022-04-13 16:02:56 -0300

    Sliders

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar.jsx'
 import SearchBar from '../../components/NavBar/SearchBar.jsx'
+import Sliders from '../../components/Sliders/Sliders.jsx';
+
 
 
 export default function Home (){
+
     return (
         <div>
             <NavBar searchBar1={true}/>
+            <Sliders />
         </div>
     )
 }
