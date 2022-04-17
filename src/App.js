094c95d94b19ba8cc0cf6b1141ab10d89be21741
Claudia <clau.neme@gmail.com>
2022-04-17 11:21:56 -0300

    Resgistro completo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from '../src/pages/Home/Home'
-import Carrito from '../src/pages/Carrito/Carrito'
-import Register from '../src/pages/Register/Register'
-import Error from '../src/components/Error/Error'
-import Footer from '../src/components/Footer/Footer'
-import Vendedor from '../src/pages/Vendedor/Vendedor'
+import { AuthProvider } from "./context/AuthContext";
+
+import Home from "../src/pages/Home/Home";
+import Carrito from "../src/pages/Carrito/Carrito";
+import Register from "../src/pages/Register/Register";
+import Error from "../src/components/Error/Error";
+import Footer from "../src/components/Footer/Footer";
+import Vendedor from "../src/pages/Vendedor/Vendedor";
+import SellerForm from './pages/UserData/SellerForm'
+import BuyerForm from './pages/UserData/BuyerForm'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/Carrito" element={<Carrito />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Profile" element={<Vendedor />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Carrito" element={<Carrito />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Profile" element={<Vendedor />} />
+          <Route path="/sellerForm" element={<SellerForm />} />
+          <Route path="/buyerForm" element={<BuyerForm />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
