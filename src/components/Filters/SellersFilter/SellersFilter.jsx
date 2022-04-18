8f0d26555a7741b167810ce022a07cd97902264f
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-18 14:02:42 -0300

    Filtro de vendedores

---
+++ src/components/Filters/SellersFilter/SellersFilter.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import SellerCard from './SellerCard';
+import { Container } from '@mui/material';
+import { getAllSellers } from '../../../redux/actions/a.seller';
+function SellersFilter() {
+    const dispatch = useDispatch();
+    const allSellers = useSelector((state) => state.allSellers);
+    const SellersData = allSellers.data
+
+    useEffect(() =>{
+        dispatch(getAllSellers())
+    },[dispatch])
+    console.log(allSellers)
+    function handleSelect(e){
+        e.preventDefault();
+        console.log('hola')
+        //dispatch(getAllProductsFrom(e.target.value))  Esta action lo que deberia hacer es filtrar los productos existentes por los productos que venda el vendedor selecionado
+    }
+    return (
+        <Container sx={{
+            marginTop: '30px',
+            height: '130px',
+            display: 'flex',
+            alignItem: 'center',
+            justifyContent: 'space-around',
+            cursor: 'pointer'
+        }}>
+            {SellersData && SellersData.map(d => <div onClick={handleSelect} value={d.value}><SellerCard name={d.name.slice(0,10)} image={d.image} /></div>)
+            }
+        </Container>
+    );
+}
+
+export default SellersFilter;
