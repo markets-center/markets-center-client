4ae30124a84042a9cf142d0e983183b091f610d3
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-19 21:45:04 -0300

    Cambio al boton de MarketsCenter para que vuelva a cargar el componente de productos en el Home

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,11 +12,13 @@ import Toolbar from '@mui/material/Toolbar';
 import { IconButton, Stack } from '@mui/material/'
 import {ShoppingCart, AccountCircle, } from '@mui/icons-material';
 import LogoutIcon from '@mui/icons-material/Logout';
-
+import { resetSliders } from '../../redux/actions/a.products'
 import { useNavigate } from 'react-router-dom'
 import {useAuth} from '../../context/AuthContext'
+import { useDispatch } from 'react-redux';
 
 export default function NavBar({searchBar1}){
+    const dispatch = useDispatch();
     const navigate = useNavigate()
     const {logout} = useAuth();
 
@@ -25,12 +27,17 @@ export default function NavBar({searchBar1}){
         navigate('/')
     }
 
+    function handleSelect(){
+        navigate('/');
+        dispatch(resetSliders())
+    }
+
     return (
         <AppBar position="static" >
             <Container maxWidth="xl">
                 <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
 
-                    <IconButton cursor="pointer" onClick={() => navigate('/')}>
+                    <IconButton cursor="pointer" onClick={handleSelect}>
                         <img src={Logo} alt="Logo" className={styles.logo} />
                     </IconButton>
                     {
