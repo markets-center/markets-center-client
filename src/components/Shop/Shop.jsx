8fa1dbacfca69c6f29b8ed085050d1d5c080400f
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-21 14:22:04 -0300

    Fix para que ordene efectivamente

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Ordenamiento from '../Ordenamiento/Ordenamiento';
 import RenderBusquedas from '../RenderBusquedas/RenderBusquedas';
 import Container from '@mui/material/Container';
 
 
 function Shop(props) {
+    const [change, setChange] = useState(0);
+    const productsSearched = useSelector(state => state.searchedProducts);
+    let firstProduct;
+
+    setTimeout(function(){
+        if(firstProduct !== productsSearched[0]._id){
+            setChange(change + 1)
+        }
+    }, 10)
     return (
         <div>
             <Container maxWidth="xl" sx={{
