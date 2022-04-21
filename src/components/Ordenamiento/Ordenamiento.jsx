0e4df9fe8e945572bc188f78a418a67f88ec86ef
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-21 00:11:36 -0300

    componente que muestra los posibles filtrados

---
+++ src/components/Ordenamiento/Ordenamiento.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+function Ordenamiento(props) {
+    return (
+        <Container maxWidth="sm" sx={{border: "1px solid red"}}>
+
+        </Container>
+    );
+}
+
+export default Ordenamiento;
