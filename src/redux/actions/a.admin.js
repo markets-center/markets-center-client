46b8559726735f5b1a147f8cb99ec846028e6295
GenaBercini <bercinigena@gmail.com>
2022-04-20 23:10:26 -0300

    Add deleteUser and UpgradeUser at actions

--- src/redux/actions/a.admin.js
+++ src/redux/actions/a.admin.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-import {ADMIN_UPDATE_ADD_CATEGORY, GET_ALL_USERS, ERRORS, MESSAGE} from './ctes'
+import {GET_ALL_CATEGORIES, ADMIN_UPDATE_ADD_CATEGORY, GET_ALL_USERS, ERRORS, MESSAGE} from './ctes'
 
 export function adminUpdateCategory (id, category) {
     return async function (dispatch) {
         try {
-            const newCategory = await axios.put(`http://localhost:4000/api/admin/categories/${id}`, category);
-            dispatch({type: MESSAGE, payload: newCategory.msg});
-            dispatch({type: ADMIN_UPDATE_ADD_CATEGORY, payload: newCategory.data})
+            const allCategories = await axios.put(`http://localhost:4000/api/admin/categories/${id}`, category);
+            dispatch({type: MESSAGE, payload: allCategories.data.msg});
+            dispatch({type: GET_ALL_CATEGORIES, payload: allCategories.data.data})
         } catch (err) {
             dispatch({type: ERRORS, payload: err.msg})
         }
@@ -17,9 +17,9 @@ export function adminUpdateCategory (id, category) {
 export function adminAddCategory (category) {
     return async function (dispatch) {
         try {
-            const newCategory = await axios.post('http://localhost:4000/api/admin/category', category);
-            dispatch({type: MESSAGE, payload: newCategory.msg})
-            dispatch({type: ADMIN_UPDATE_ADD_CATEGORY, payload: newCategory.data})
+            const allCategories = await axios.post('http://localhost:4000/api/admin/category', category);
+            dispatch({type: MESSAGE, payload: allCategories.data.msg})
+            dispatch({type: GET_ALL_CATEGORIES, payload: allCategories.data.data})
         } catch (err) {
             dispatch({type: ERRORS, payload: err.msg})
         }
@@ -30,7 +30,8 @@ export function adminDeleteCategory (id) {
     return async function (dispatch) {
         try {
             const response = await axios.delete(`http://localhost:4000/api/admin/categories/${id}`);
-            dispatch({type: MESSAGE, payload: response.msg})
+            dispatch({type:GET_ALL_CATEGORIES, payload:response.data.data})
+            dispatch({type: MESSAGE, payload: response.data.msg})
         } catch (err) {
             dispatch({type: ERRORS, payload: err.msg})
         }
@@ -44,6 +45,29 @@ export function getAllUsers(){
             dispatch({type: MESSAGE, payload: users.data.msg})
             dispatch({type: GET_ALL_USERS, payload: users.data.data})
 
+        } catch (err) {
+            dispatch({type: ERRORS, payload: err.msg})
+        }
+    }
+}
+
+
+export function deleteUser(id) {
+    return async function (dispatch) {
+        try {
+            const result = await axios.delete(`http://localhost:4000/api/admin/userDelete/${id}`);
+            dispatch({type: MESSAGE, payload: result.data.msg})
+        } catch (err) {
+            dispatch({type: ERRORS, payload: err.msg})
+        }
+    }
+}
+
+export function upgradeUser(id){
+    return async function (dispatch) {
+        try {
+            const result = await axios.put(`http://localhost:4000/api/admin/userAdmin/${id}`);
+            dispatch({type: MESSAGE, payload: result.data.msg})
         } catch (err) {
             dispatch({type: ERRORS, payload: err.msg})
         }
