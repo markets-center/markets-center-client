cd0bf212d3e426297bdfa15223480a4869e4f061
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-22 12:24:15 -0300

    cambios en redux por nueva ruta en el back. agregado de estados globales y logica de ordenamiento y filtrado

--- src/redux/functions/functions.js
+++ src/redux/functions/functions.js
@@ -1,48 +1,52 @@
 
 
-export function orderByPrice(payload, state){
-    let sorted = payload === 'low' ?
-            state.sort(function(a,b){
-                if(a.price > b.price){
-                    return 1;
-                }
-                if(b.price > a.price){
-                    return -1;
-                }
-                return 0;
-            }) :
-            state.sort(function(a,b){
-                if(a.price > b.price){
-                    return -1;
-                }
-                if(b.price > a.price){
-                    return 1;
-                }
-                return 0;
-            })
+export function order(payload,state){
+    let sorted = state
+    if(payload === 'low'){
+        state.sort(function(a,b){
+            if(a.price > b.price){
+                return 1;
+            }
+            if(b.price > a.price){
+                return -1;
+            }
+            return 0;
+        })
+    }
+    if(payload === 'high'){
+        state.sort(function(a,b){
+            if(a.price > b.price){
+                return -1;
+            }
+            if(b.price > a.price){
+                return 1;
+            }
+            return 0;
+        }) 
+    }  
+    if(payload === 'a-z'){
+        state.sort(function(a,b){
+            if(a.name.toLowerCase() > b.name.toLowerCase()){
+                return 1;
+            }
+            if(b.name.toLowerCase() > a.name.toLowerCase()){
+                return -1;
+            }
+            return 0;
+        })
+    }
+    if(payload === 'z-a'){
+        state.sort(function(a,b){
+            if(a.name.toLowerCase() > b.name.toLowerCase()){
+                return -1;
+            }
+            if(b.name.toLowerCase() > a.name.toLowerCase()){
+                return 1;
+            }
+            return 0;
+        })
+    }
     return sorted
-}
-export function orderByAlph(payload,state){
-    let sortedArr = payload === 'a-z' ?
-            state.sort(function(a,b){
-                if(a.name.toLowerCase() > b.name.toLowerCase()){
-                    return 1;
-                }
-                if(b.name.toLowerCase() > a.name.toLowerCase()){
-                    return -1;
-                }
-                return 0;
-            }) :
-            state.sort(function(a,b){
-                if(a.name.toLowerCase() > b.name.toLowerCase()){
-                    return -1;
-                }
-                if(b.name.toLowerCase() > a.name.toLowerCase()){
-                    return 1;
-                }
-                return 0;
-            })
-    return sortedArr
 }
 
 export function filterByPrice(payload,state){
@@ -50,21 +54,34 @@ export function filterByPrice(payload,state){
 
     if(payload === '0-500'){
         filter = state.filter(s=> s.price >= '0' && s.price < '500');
+        if(!filter.length){
+            alert('No se encuentra productos con en este rango')
+       }
         return filter;
     }
     if(payload === '500-1500'){
         filter = state.filter(s=> s.price >= '500' && s.price < '1500');
+        if(!filter.length){
+            alert('No se encuentra productos con en este rango')
+       }
         return filter;
     }
     if(payload === '1500-3000'){
         filter = state.filter(s=> s.price >= '1500' && s.price < '3000');
+        if(!filter.length){
+            alert('No se encuentra productos con en este rango')
+       }
         return filter;
     }
     if(payload === '>3000'){
         filter = state.filter(s=> s.price >= '3000');
+        if(!filter.length){
+             alert('No se encuentra productos con en este rango')
+        }
         return filter;
     }
-    return state;
+
+    return filter;
 
     
 
