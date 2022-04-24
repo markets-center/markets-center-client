dee7e374efa60fe40f07e63f709166036a57160b
GenaBercini <bercinigena@gmail.com>
2022-04-23 21:07:56 -0300

    Jesus

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -3,8 +3,6 @@ import image from './Error2.gif'
 export default function Error(){
     return (
         <div>
-            <img src={image} alt="Error"/>
-            <h4>Se rompio todo</h4>
         </div>
     )
 }
