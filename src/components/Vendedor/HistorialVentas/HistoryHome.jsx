9ff5e2d1ea90e5ab190f148d0978f871fc062d5a
Tomas Bohn Guixeras <bohnguixeras@gmail.com>
2022-04-24 00:39:15 -0300

    orders history of seller

--- src/components/Vendedor/HistorialVentas/HistoryHome.jsx
+++ src/components/Vendedor/HistorialVentas/HistoryHome.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ordersBySeller } from '../../../redux/actions/a.seller.js'
+import {useNavigate} from 'react-router-dom'
 import { useAuth } from '../../../context/AuthContext.js'
 import ListItem from './ListItem.jsx'
 
+import NavBar from '../../NavBar/NavBar.jsx'
+import {Button} from '@mui/material'
+
 
 export default function HistoryHome(){
+    const navigate = useNavigate()
     const {oneUser} = useAuth()
     const history = useSelector(state => state.history)
     const dispatch = useDispatch()
@@ -14,6 +19,19 @@ export default function HistoryHome(){
     }, [dispatch, oneUser._id])
     return (
         <div>
+            <NavBar />
+            <Button 
+                        onClick={() => navigate('/Login')}
+                        variant="contained" 
+                        color="primary" 
+                        sx={{
+                            m: 2,
+                            left: '1240px',
+                            fontWeight: '600',
+                        }}
+                    >
+                        Volver
+                    </Button>
             {
             history.map( order => {
                 return (
--- src/components/Vendedor/HistorialVentas/ListItem.jsx
+++ src/components/Vendedor/HistorialVentas/ListItem.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
+
+
 import styles from './ListItem.module.css'
 import DefaultUser from '../../../images/defaultUser.png'
+import { IconButton } from '@mui/material'
+import { AddBox } from '@mui/icons-material'
 
 export default function ListItem({element}){
-    // console.log(props)
+    console.log(element)
     const clientName = element.userId
-    console.log(clientName)
+    // console.log(clientName)
     return (
         <div className={styles.container}>
-            <img 
-            src={clientName.image} 
-            alt='Client' 
-            className={styles.image}
-            onError={(e)=>{
-                e.target.onerror = null
-                e.target.src = DefaultUser}}
-            />
-            <h3>{clientName.name}</h3>
+            <div className={styles.left}>
+                <img 
+                src={clientName.image} 
+                alt='Client' 
+                className={styles.image}
+                onError={(e)=>{
+                    e.target.onerror = null
+                    e.target.src = DefaultUser}}
+                />
+                <div>
+                    <h3>{clientName.name}</h3>
+                    <p>Mail: {clientName.email}</p>
+                    <p>Tel: {clientName.phone}</p>
+                </div>
+            </div>
+            <div className={styles.right}>
+                <p>{`Estado: ${element.status}`}</p>
+                <p>{`Total: $${element.amount}`}</p>
+                <IconButton>
+                    <AddBox />
+                </IconButton>
+            </div>
         </div>
     )
 }
