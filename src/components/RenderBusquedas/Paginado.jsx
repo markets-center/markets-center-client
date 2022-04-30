372be95aa417f2fdd63abf4fb03ecdfc5e8a6608
Ignacio Diaz <marketscenter.pg@gmail.com>
2022-04-29 23:49:10 -0300

    .

--- src/components/RenderBusquedas/Paginado.jsx
+++ src/components/RenderBusquedas/Paginado.jsx
@@ -1,32 +1,44 @@
-import React from "react";
-import s from './Paginado.module.css'
+import React, { useState }  from "react";
+import s from './Paginado.module.css';
+
 export default function Paginado({ products, setCurrent, current }){
   const pageNumbers = [];
   const pageCount = Math.ceil(products.length / 13);
   for (let i = 1; i < pageCount + 1; i++) {
     pageNumbers.push(i);
   }
+  const [clicked, setClicked] = useState(1);
+
+  function handleClick(n){
+      
+      setCurrent(n);
+      setClicked(n);
+  }
+
+  
   return (
     <div className={s.paginadoContainer}>
-       { current!== 1?<div>
-            <button onClick={() => {current=== 1? setCurrent(current): setCurrent(current - 1)}} className={s.buttonNextPrev}>
-              {'<'}
+        <div className={s.divs}>
+            <button onClick={() => {current=== 1? setCurrent(current): setCurrent(current - 1)}} className={s.buttonNextPrev} >
+                &laquo;
             </button>
-        </div>: ""}
+        </div>
       {pageNumbers?.map((n) => {
         return (
-          <div key={n}>
-            <button onClick={() => setCurrent(n)} className={s.buttonPaginado}>
+          <div key={n} className={s.divs}>
+            {clicked === n?<button onClick={() =>handleClick(n)} className={s.buttonPaginadoSelect} >
               {n}
-            </button>
+            </button>:<button onClick={() => handleClick(n)} className={s.buttonPaginado} >
+              {n}
+            </button>}
           </div>
         );
       })}
-        { current !== pageNumbers.length?<div>
+        <div className={s.divs}>
             <button onClick={() => {current=== pageNumbers.length? setCurrent(current): setCurrent(current + 1)}} className={s.buttonNextPrev}>
-            {'>'}
+            &raquo;
             </button>
-        </div>: ""}
+        </div>
     </div>
   );
 };
