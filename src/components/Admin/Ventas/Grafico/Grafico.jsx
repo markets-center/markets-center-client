d86633f0012b4ce54433e47bc35d281100e18fac
Tomas Bohn Guixeras <bohnguixeras@gmail.com>
2022-05-01 15:13:14 -0300

    update

--- src/components/Admin/Ventas/Grafico/Grafico.jsx
+++ src/components/Admin/Ventas/Grafico/Grafico.jsx
@@ -13,53 +13,58 @@ import {
 export default function Home({history}) {
     console.log(history);
     return (
-        <div className={styles.AreaChart}>
-            <h4>Promedio de ventas</h4>
-            <ResponsiveContainer width="99%" height={400} >
-                <AreaChart data={history} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                    <defs>
-                        <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
-                            <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
-                        </linearGradient>
-                    </defs>
-                
-                <Area dataKey="monto" stroke="#2451B7" fill="url(#color)" />
-                
-                <XAxis
-                    dataKey="id"
-                    axisLine={false}
-                    tickLine={false}
-                    // tickFormatter={(str) => {
-                    //     return `operación ${str}`;
-                    // }}
-                />
-            
-                <YAxis
-                    datakey="monto"
-                    axisLine={false}
-                    tickLine={false}
-                    tickCount={8}
-                    tickFormatter={(number) => `$${number.toFixed(1)}`}
-                />
-            
-                {/* <Tooltip content={<CustomTooltip />} /> */}
+        <>
+        {
+            history.length > 0 &&
+            <div className={styles.AreaChart}>
+                <h4>Promedio de ventas</h4>
+                <ResponsiveContainer width="99%" height={400} >
+                    <AreaChart data={history} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                        <defs>
+                            <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
+                                <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+                            </linearGradient>
+                        </defs>
                     
-                <CartesianGrid opacity={0.1} vertical={false} />
-                </AreaChart>
-            </ResponsiveContainer>
-        </div>
+                    <Area dataKey="monto" stroke="#2451B7" fill="url(#color)" />
+                    
+                    <XAxis
+                        dataKey="id"
+                        axisLine={false}
+                        tickLine={false}
+                        // tickFormatter={(str) => {
+                        //     return `operación ${str}`;
+                        // }}
+                    />
+                
+                    <YAxis
+                        datakey="monto"
+                        axisLine={false}
+                        tickLine={false}
+                        tickCount={8}
+                        tickFormatter={(number) => `$${number.toFixed(1)}`}
+                    />
+                
+                    <Tooltip content={<CustomTooltip />} />
+                        
+                    <CartesianGrid opacity={0.1} vertical={false} />
+                    </AreaChart>
+                </ResponsiveContainer>
+            </div>
+        }
+        </>
     );
 }
 
-// function CustomTooltip({ active, payload, label }) {
-//     if (active) {
-//     return (
-//         <div className="tooltip">
-//         <h4>{`ID ${label}`}</h4>
-//         <p>Total: U${payload[0].value.toFixed(1)}</p>
-//         </div>
-//     );
-//     }
-//     return null;
-// }
+function CustomTooltip({ active, payload, label }) {
+    if (active) {
+    return (
+        <div className="tooltip">
+        <h4>{`ID ${label}`}</h4>
+        <p>Total: U${payload[0].value.toFixed(1)}</p>
+        </div>
+    );
+    }
+    return null;
+}
