1cbdb787821fd6ef2bbb7006a993c8ddbe8d1794
FNahuelO <franconawel@gmail.com>
2022-05-02 13:32:04 -0300

    varios

--- src/components/Filters/SellersFilter/SellersFilter.jsx
+++ src/components/Filters/SellersFilter/SellersFilter.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import SellerCard from './SellerCard';
 import { Container } from '@mui/material';
 import { getAllSellers } from '../../../redux/actions/a.seller';
+import style from './SellersFilter.module.css'
 
 function SellersFilter() {
     const dispatch = useDispatch();
@@ -13,18 +14,7 @@ function SellersFilter() {
     },[dispatch])
 
     return (
-        <Container sx={{
-            marginTop: '40px',
-            height: 'auto',
-            display: 'flex',
-            alignItem: 'center',
-            justifyContent: 'space-around',
-            wordWrap: 'break-word',
-            overflowX: 'scroll',
-            overflowY: 'hidden',
-            whiteSpace: 'normal',
-        }}>
-
+        <Container className={style.container}>
             {allSellers && allSellers.map(d => <SellerCard key={d._id} name={d.name.slice(0,10)} image={d.image} id={d._id}/>)
             }
         </Container>
