9d2c521f029938d4ac946d587feb731012bccca2
Daniela Bedoya Poveda <danielapoveda75@gmail.com>
2022-05-02 14:44:18 -0500

    Validaciones favoritos front

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,45 +1,45 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import NavBar from '../../components/NavBar/NavBar.jsx'
-import {delAlert} from '../../redux/actions/a.alert'
+import { delAlert } from '../../redux/actions/a.alert'
 
 import Slider2 from '../../components/Sliders/Slider2.jsx';
 import SellerFilter from './../../components/Filters/SellersFilter/SellersFilter';
 import Shop from '../../components/Shop/Shop';
-import {Snackbar} from '@mui/material';
-import {SnackbarAlert} from '../../components/Alert/success';
+import { Snackbar } from '@mui/material';
+import { SnackbarAlert } from '../../components/Alert/success';
 
-export default function Home (){
+export default function Home() {
     const [render, setRender] = useState(false)
     const dispatch = useDispatch()
     const productsSearched = useSelector(state => state.searchedProducts);
     const initialProducts = useSelector(state => state.allProducts);
     const alert = useSelector((state) => state.alert);
 
-    function handleClose(){
+    function handleClose() {
         dispatch(delAlert())
     }
 
-    setTimeout(function(){
-        if(productsSearched.length < initialProducts.length){
+    setTimeout(function () {
+        if (productsSearched.length < initialProducts.length) {
             setRender(true)
         }
-        if(productsSearched.length === initialProducts.length){
+        if (productsSearched.length === initialProducts.length) {
             setRender(false)
         }
     }, 1);
-    
+
     useEffect(() => {
-        if(!localStorage.getItem('productsTemp')){
+        if (!localStorage.getItem('productsTemp')) {
             localStorage.setItem('productsTemp', '[]')
         }
-    },[])
+    }, [])
 
     return (
         <div>
-            <NavBar searchBar={true} home={true}/>
+            <NavBar searchBar={true} home={true} carrito={true} />
             <SellerFilter />
-            {render?<Shop />:<Slider2 />}
+            {render ? <Shop /> : <Slider2 />}
 
             <Snackbar open={!!alert} autoHideDuration={4000} onClose={handleClose} anchorOrigin={{
                 vertical: 'bottom',
@@ -49,7 +49,7 @@ export default function Home (){
                     {alert}
                 </SnackbarAlert>
             </Snackbar>
-            
+
         </div>
     )
 }
