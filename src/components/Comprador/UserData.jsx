409eb0a618fd2f5bf04fd96c5db56ecea8374a98
FNahuelO <franconawel@gmail.com>
2022-05-02 13:21:50 -0300

    comprador

--- src/components/Comprador/UserData.jsx
+++ src/components/Comprador/UserData.jsx
@@ -1,38 +1,14 @@
 import React, { useState } from "react";
-import { Container, Box, Typography, Button } from "@mui/material";
+import { Container, Box, Typography, Button, Modal } from "@mui/material";
 
 // import { useAuth } from "../../context/AuthContext";
 import { useSelector } from "react-redux";
-import ResetPass from './ResetPass'
+import ResetPass from './ResetPass';
+import style from './Style/User.module.css'
 
 // import { useNavigate } from "react-router-dom";
-import Modal from '@mui/material/Modal';
 import UserForm from './UserForm'
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 800,
-  bgcolor: 'background.paper',
-  border: '1px solid #000',
-  borderRadius: '10px',
-  boxShadow: 24,
-  p: 4,
-};
-const stylePass = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '550px',
-  bgcolor: 'background.paper',
-  border: '1px solid #000',
-  borderRadius: '10px',
-  boxShadow: 24,
-  p: 4,
-};
 
 export default function UserData() {
   //   const { oneUser } = useAuth();
@@ -51,24 +27,14 @@ export default function UserData() {
   return (
     <>
       <Container sx={{ borderBottom: "2px solid black" }}>
-        <Container
+      <Container className={style.containerData}
           sx={{
-            height: "250px",
             display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
           }}
         >
-          <Container
-            sx={{
-              width: "100px",
-              height: "200px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
+          <Container className={style.title}>
             <Box sx={{
+              marginBottom: "25px"
             }}>
               <Typography variant='h6' sx={{
                 fontWeight: 'bold'
@@ -84,15 +50,7 @@ export default function UserData() {
               />
             </Box>
           </Container>
-          <Container
-            sx={{
-              width: "1400px",
-              height: "200px",
-              margin: "0 50px",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
+          <Container className={style.containerInfo}>
             <Container
               sx={{
                 height: "160px",
@@ -129,15 +87,7 @@ export default function UserData() {
             </Container>
           </Container>
         </Container>
-        <Container
-          sx={{
-            height: "60px",
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            mb: 4,
-          }}
-        >
+        <Container className={style.botones}>
           <Button
             onClick={handleOpen}
             variant="contained"
@@ -162,7 +112,7 @@ export default function UserData() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
+        <Box className={style.containerForm}>
           <UserForm
             name={oneUser.name}
             email={oneUser.email}
@@ -181,7 +131,7 @@ export default function UserData() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={stylePass}>
+        <Box className={style.containerPass}>
           <ResetPass
             name={oneUser.name}
             email={oneUser.email}
