8f4a25d775ed1fb84f10d5d059d6219bb73231a4
FNahuelO <franconawel@gmail.com>
2022-05-02 13:52:20 -0300

    vendedor2

--- src/components/Vendedor/AddProduct/AddProduct.jsx
+++ src/components/Vendedor/AddProduct/AddProduct.jsx
@@ -14,21 +14,6 @@ const Input = styled('input')({
 });
 
 export default function AddProduct({ input, setInput, handleClose, open, prodId, handleSubmit }) {
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 1000,
-        height: 500,
-        bgcolor: 'background.paper',
-        border: '1px solid #000',
-        borderRadius: 6,
-        boxShadow: 24,
-        p: 4,
-        display: "flex",
-        flexDirection: 'row',
-    };
     // const { currentUser } = useAuth();
 
     const dispatch = useDispatch();
@@ -114,7 +99,7 @@ export default function AddProduct({ input, setInput, handleClose, open, prodId,
             open={open}
             onClose={handleClose}
         >
-            <Box sx={{ ...style }} component="form">
+            <Box className={styles.container} component="form">
                 <div className={styles.mainDiv}>
                     <Typography variant="h4" gutterBottom component="div">
                         Agregar un producto:
